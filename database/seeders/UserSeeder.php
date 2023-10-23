e3109c80aa44bef917f58964a6622e8498d2792f
Aliaksei <ramone009@gmail.com>
2023-10-23 18:37:37 +0300

    Added User Seeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => env('DEFAULT_ADMIN_NAME'),
+            'email' => env('DEFAULT_ADMIN_EMAIL'),
+            'email_verified_at' => now(),
+            'password' => Hash::make(env('DEFAULT_ADMIN_PASSWORD')),
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
