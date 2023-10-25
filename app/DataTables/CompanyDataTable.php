c78a131edc695ada16af4ac034f2beb73186cc94
Aliaksei <ramone009@gmail.com>
2023-10-25 20:13:00 +0300

    Added datatables for companies and employees.

---
+++ app/DataTables/CompanyDataTable.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\DataTables;
+
+use App\Models\Company as Model;
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class CompanyDataTable extends DataTable
+{
+    /**
+     * Build the DataTable class.
+     *
+     * @param QueryBuilder $query Results from query() method.
+     */
+    public function dataTable(QueryBuilder $query): EloquentDataTable
+    {
+        return (new EloquentDataTable($query))
+            ->addColumn(Model::COLUMN_LOGO, function ($data){
+                $logo = $data->logo;
+                return $logo ? "<img src=/storage/images/logo/$logo/>" : null;
+            })
+            ->addColumn('action', 'companies.action')
+            ->rawColumns(['logo', 'action'])
+            ->setRowId('id');
+    }
+
+    /**
+     * Get the query source of dataTable.
+     */
+    public function query(Model $model): QueryBuilder
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use the html builder.
+     */
+    public function html(): HtmlBuilder
+    {
+        return $this->builder()
+                    ->setTableId('company-table')
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    ->orderBy(1)
+                    ->selectStyleSingle()
+                    ->buttons([
+                        Button::make('add'),
+                        Button::make('excel'),
+                        Button::make('reload')
+                    ]);
+    }
+
+    /**
+     * Get the dataTable columns definition.
+     */
+    public function getColumns(): array
+    {
+        return [
+            Column::make('id'),
+            Column::make(Model::COLUMN_LOGO),
+            Column::make(Model::COLUMN_NAME),
+            Column::make(Model::COLUMN_EMAIL),
+            Column::make(Model::COLUMN_URL),
+            Column::make(Model::COLUMN_PHONE),
+            Column::computed('action')
+                ->exportable(false)
+                ->printable(false)
+                ->width(60)
+                ->addClass('text-center'),
+        ];
+    }
+
+    /**
+     * Get the filename for export.
+     */
+    protected function filename(): string
+    {
+        return 'Company_' . date('YmdHis');
+    }
+}
---
+++ app/DataTables/EmployeeDataTable.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\Employee as Model;
+use Illuminate\Database\Eloquent\Builder as QueryBuilder;
+use Yajra\DataTables\EloquentDataTable;
+use Yajra\DataTables\Html\Builder as HtmlBuilder;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class EmployeeDataTable extends DataTable
+{
+    /**
+     * Build the DataTable class.
+     *
+     * @param QueryBuilder $query Results from query() method.
+     */
+    public function dataTable(QueryBuilder $query): EloquentDataTable
+    {
+        return (new EloquentDataTable($query))
+            ->addColumn('action', 'employees.action')
+            ->rawColumns(['action'])
+            ->setRowId('id');
+    }
+
+    /**
+     * Get the query source of dataTable.
+     */
+    public function query(Model $model): QueryBuilder
+    {
+        return $model->newQuery()->with(['company']);
+    }
+
+    /**
+     * Optional method if you want to use the html builder.
+     */
+    public function html(): HtmlBuilder
+    {
+        return $this->builder()
+                    ->setTableId('employee-table')
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    ->orderBy(1)
+                    ->selectStyleSingle()
+                    ->buttons([
+                        Button::make('add'),
+                        Button::make('excel'),
+                        Button::make('reload')
+                    ]);
+    }
+
+    /**
+     * Get the dataTable columns definition.
+     */
+    public function getColumns(): array
+    {
+        return [
+            Column::make('id'),
+            Column::make(Model::COLUMN_FIRST_NAME),
+            Column::make(Model::COLUMN_LAST_NAME),
+            'Company' => new Column([
+                'title' => 'Company',
+                'data' => 'company.name',
+                'name' => 'company.name'
+            ]),
+            Column::make(Model::COLUMN_EMAIL),
+            Column::make(Model::COLUMN_PHONE),
+            Column::make(Model::COLUMN_NOTE),
+            Column::computed('action')
+                ->exportable(false)
+                ->printable(false)
+                ->width(60)
+                ->addClass('text-center'),
+        ];
+    }
+
+    /**
+     * Get the filename for export.
+     */
+    protected function filename(): string
+    {
+        return 'Employee_' . date('YmdHis');
+    }
+}
