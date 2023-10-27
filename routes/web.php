3dc924be51002022b41724ad2d99b92ff2b61fb5
Aliaksei <ramone009@gmail.com>
2023-10-27 15:02:43 +0300

    Updated routes.

--- routes/web.php
+++ routes/web.php
@@ -4,22 +4,10 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\CompanyController;
 use App\Http\Controllers\EmployeeController;
+use Illuminate\Support\Facades\Auth;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/', function () {
+    return redirect('login');
 });
 
 Route::group([
@@ -29,12 +17,8 @@ Route::group([
 ], function() {
 
     Route::get('/', function () {
-        return view('welcome');
-    });
-
-    Route::get('/dashboard', function () {
         return view('dashboard');
-    })->middleware(['auth', 'verified'])->name('dashboard');
+    })->middleware(['auth', 'verified']);
 
     Route::resource(
         'companies',
@@ -47,8 +31,12 @@ Route::group([
         EmployeeController::class,
         ['except' => ['show']]
     )->middleware(['auth', 'verified']);
+});
 
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 require __DIR__.'/auth.php';
